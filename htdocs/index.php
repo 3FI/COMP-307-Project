ecba443c8cfcb4423e0bd9e95795a57a19dbfdc3
EFI <ec10pc8@gmail.com>
2023-12-08 13:39:03 -0500

    Routing for SOCS

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,7 +1,6 @@
 <?php
-	$request = $_SERVER['REQUEST_URI'];
+	$request = str_replace("/~ecadot", "", $_SERVER['REQUEST_URI']);
 	$viewDir = '/cgi-bin/';
-
 	switch ($request) {
 	    case '':
 	    case '/':
